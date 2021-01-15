6268f0cd1de8e65466366040eb224839131c4158
Superfan1995 <xf4@ualberta.ca>
2021-01-14 23:22:19 -0700

    python script

---
+++ lab1.py
@@ -0,0 +1,10 @@
+import requests
+
+###########################
+# Main code starts here
+###########################
+
+#response = requests.get('https://github.com/Superfan1995', auth=('superfan1995', '13905359980'))
+#version = response.raw.version
+
+print(requests.__version__)
